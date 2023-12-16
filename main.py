f91d1a248357876b7545c9abefc1c068c54ca817
nastya <Nishhiory@yandex.ru>
2023-12-16 16:27:45 +0300

    2commit

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def print_hello(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     print_hi('PyCharm')
 
